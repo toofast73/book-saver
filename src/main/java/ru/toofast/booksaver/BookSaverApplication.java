944ae7fca19d8d2ed372d084f4d052cebab8d225
nemoedelo <elenakulagina.mail@gmail.com>
2020-05-08 19:59:05 +0300

    Initial commit

---
+++ src/main/java/ru/toofast/booksaver/BookSaverApplication.java
@@ -0,0 +1,13 @@
+package ru.toofast.booksaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/toofast/booksaver/BookSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.toofast.booksaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
